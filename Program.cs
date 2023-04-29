fdb3f4485f836e6b3de5398d131a64cb8a9a3869
Jefferson Savidotti <jeffersonsavidotti@gmail.com>
2023-04-29 17:47:46 -0300

    Programa que interage com uma calculadora web.
    Abaixo apresentamos um descritivo do desenvolvimento do programa e um tutorial de utilização da ferramenta de teste, onde são verificados os resultados de operações aritméticas em uma calculadora on line.
    No desenvolvimento do software, criamos um projeto na linguagem C# (dotnet new console), na IDE “Visual Studio Code”. Após, instalamos a ferramenta de teste “dotnet add package Selenium.WebDriver --version 4.9.0”. Na sequência, foram Inicializadas as bibliotecas “using OpenQA.Selenium” e “using OpenQA.Selenium.Chrome”. 
     
    
    Após, foi elaborado a codificação principal, onde foram definidas as entradas de dados, ou seja, os números que serão calculados e a operação aritmética a ser realizada. Também foi definido o resultado esperado, para que o programa compare com o retorno da calculadora on line e retorne ao usuário se o “teste passou” ou o “teste falhou”.
    
     
    	Para utilização do programa, é necessário que o usuário informe o ID dos números que serão calculados (linhas 20 e 22); a operação aritmética que a ser realizada (linha 21) e o resultado esperado (linha 37). 
    	Ao executar o programa, o site de teste é acessado, localizado os elementos da página, realizado as interações do usuário, verificado o resultado e o navegador é fechado.
    	 
     
    
    Os retornos do programa para o usuário são: “teste passou”, caso o retorno da calculadora seja igual ao resultado esperado inserido pelo usuário, ou “teste falhou” caso o retorno da calculadora seja diferentedo resultado esperado inserido pelo usuário.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class TesteSelenium
+{
+    static void Main(string[] args)
+    {
+        // Configura o ChromeDriver
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--start-maximized");
+        IWebDriver driver = new ChromeDriver(options);
+
+        // Define a URL da página de teste
+        string url = "https://www.calculadoraonline.com.br/calculadora-virtual-gratis";
+
+        // Acessa a página de teste
+        driver.Navigate().GoToUrl(url);
+
+        // Localiza os elementos da página
+        IWebElement botao2 = driver.FindElement(By.Id("b18"));
+        IWebElement botaoMais = driver.FindElement(By.Id("b4"));
+        IWebElement botao3 = driver.FindElement(By.Id("b19"));
+        IWebElement botaoIgual = driver.FindElement(By.Id("b27"));
+        IWebElement campoResultado = driver.FindElement(By.Id("TIExp"));
+
+        // Realiza as interações do usuário
+        botao2.Click();
+        Thread.Sleep(3000);
+        botaoMais.Click();
+        Thread.Sleep(3000);
+        botao3.Click();
+        Thread.Sleep(3000);
+        botaoIgual.Click();
+        Thread.Sleep(3000);
+
+        // Verifica o resultado
+        string resultadoEsperado = "5";
+        string resultadoObtido = campoResultado.GetAttribute("value");
+        if (resultadoObtido == resultadoEsperado)
+        {
+            Console.WriteLine("Teste passou!");
+        }
+        else
+        {
+            Console.WriteLine("Teste falhou!");
+        }
+
+        // Fecha o navegador
+        driver.Quit();
+    }
+}
